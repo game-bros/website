ca87bc3aea9adbf7a723fe1b98287ff5e95c8b57
Tero Malkki <tero.malkki@gmail.com>
2020-11-11 07:59:29 +0200

    Add bros

---
+++ src/components/Bro.tsx
@@ -0,0 +1,72 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const BroBase = styled.div<BroBaseProps>`
+  background-color: ${props => props.backgroundColor};
+  padding: 10px 25px;
+  position: relative;
+  min-height: 150px;
+  color: white;
+
+  .avatar {
+    position: absolute;
+    bottom: 0;
+    right: 5px;
+  }
+
+  .name {
+    letter-spacing: 2px;
+    margin: 0;
+    text-transform: uppercase;
+    text-shadow: 1px 1px 4px rgba(0, 0, 0, 0.5);
+  }
+
+  .title {
+    font-size: 1.1em;
+    font-weight: 600;
+    letter-spacing: 2px;
+    opacity: 0.9;
+  }
+
+  .title::before,
+  .title::after {
+    content: '"';
+  }
+
+  .role {
+    position: absolute;
+    left: 25px;
+    bottom: 20px;
+    text-transform: uppercase;
+    font-weight: 600;
+  }
+`;
+type BroBaseProps = {
+  readonly backgroundColor: string;
+}
+
+type BroProps = {
+  /** background color of the bro card */
+  readonly backgroundColor: string;
+  /** real name */
+  readonly name: string;
+  /** alias */
+  readonly title: string;
+  /** what the person does in the studio */
+  readonly role: string;
+  /** Optional. avatar of the bro card */
+  readonly avatar?: string;
+}
+
+const Bro: React.FC<BroProps> = ({ backgroundColor, name, title, role, avatar }) => {
+  return (
+    <BroBase backgroundColor={backgroundColor}>
+      <h2 className="name">{name}</h2>
+      <div className="title">{title}</div>
+      <div className="role">{role}</div>
+      {avatar && <img className="avatar" src={avatar} alt={title} />}
+    </BroBase>
+  );
+}
+
+export default Bro;
--- src/pages/Bros.tsx
+++ src/pages/Bros.tsx
@@ -1,9 +1,50 @@
 import React from 'react';
+import styled from 'styled-components';
+import Bro from '../components/Bro';
+import ContentWrapper from '../components/common/ContentWrapper';
+import Page from '../components/common/Page';
 
-const Bros = () => {
+const BroContainer = styled.div`
+  display: flex;
+  margin: 0 -10px;
+  flex-flow: row wrap;
+
+  > div {
+    flex: 0 1 38%;
+    margin: 10px;
+  }
+
+  @media only screen and (max-width: 600px) {
+    flex-direction: column;
+  }
+`;
+
+function Bros() {
   return (
-    <div>Bros</div>
+    <Page title="Bros" titleBackground="rgb(148, 92, 209)">
+      <ContentWrapper>
+        <main>
+          <p>GameBros is an independent game studio founded in 2007 by two brothers, Sensei and Batzy.</p>
+          <BroContainer>
+            <Bro name="Tero Malkki"
+                  title="Sensei"
+                  role="Developer"
+                  avatar="/img/sensei.png"
+                  backgroundColor="rgb(209, 92, 92)"/>
+            <Bro name="Toni Malkki"
+                  title="Batzy"
+                  role="Artist"
+                  avatar="/img/batzy.png"
+                  backgroundColor="rgb(94, 92, 209)" />
+            <Bro name="Rasmus Malkki"
+                  title="Kai"
+                  role="Tester"
+                  backgroundColor="rgb(142, 209, 92)" />
+          </BroContainer>
+        </main>
+      </ContentWrapper>
+    </Page>
   );
-};
+}
 
 export default Bros;
