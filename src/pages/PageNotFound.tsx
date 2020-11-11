e973c23e7ad015894abcce71898558ee7385ad54
Tero Malkki <tero.malkki@gmail.com>
2020-11-11 16:08:52 +0200

    404 error page

--- src/pages/PageNotFound.tsx
+++ src/pages/PageNotFound.tsx
@@ -1,9 +1,17 @@
 import React from 'react';
+import ContentWrapper from '../components/common/ContentWrapper';
+import Page from '../components/common/Page';
 
-const PageNotFound = () => {
+ const PageNotFound = () => {
   return (
-    <div>404</div>
+    <div>
+      <Page title="404 Not Found" titleBackground="rgb(148, 92, 209)">
+        <ContentWrapper>
+          <p>Page not found.</p>
+        </ContentWrapper>
+      </Page>
+    </div>
   );
 }
 
-export default PageNotFound
+export default PageNotFound;
