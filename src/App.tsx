6987134bf64ee9452dd1a46c42bd802c79468b98
Tero Malkki <tero.malkki@gmail.com>
2020-11-11 21:20:25 +0200

    Added pages for games + fontawesome

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import { Route, Switch } from 'react-router-dom';
 import Blog from './pages/Blog';
 import Bros from './pages/Bros';
 import Games from './pages/Games';
+import ASWX from './pages/games/ASWX';
+import CoolBombers from './pages/games/CoolBombers';
 import Home from './pages/Home';
 import PageNotFound from './pages/PageNotFound';
 
@@ -10,7 +12,9 @@ const App = () => {
   return (
     <div className="App">
       <Switch>
-        <Route exact path="/" component={Home} />
+      <Route exact path="/" component={Home} />
+        <Route exact path="/games/coolbombers" component={CoolBombers} />
+        <Route exact path="/games/aswx" component={ASWX} />
         <Route exact path="/games" component={Games} />
         <Route exact path="/bros" component={Bros} />
         <Route exact path="/blog" component={Blog} />
---
+++ src/components/common/Screenshot.tsx
@@ -0,0 +1,74 @@
+import React, { useState } from 'react';
+import styled from 'styled-components';
+
+const ScreenshotBase = styled.div`
+  .screenshot {
+    display: inline-block;
+    width: 100%;
+    height: 100%;
+    cursor: pointer;
+
+    img {
+      width: 100%;
+    }
+  }
+
+  .lightbox {
+    background-color: rgba(0, 0, 0, 0.5);
+    position: fixed;
+    z-index: 2000;
+    margin: 0 auto;
+    top: 0;
+    left: 0;
+    text-align: center;
+    vertical-align: center;
+    width: 100vw;
+    height: 100vh;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-evenly;
+
+    .wrapper {
+      max-height: 90vh;
+    }
+
+    img {
+      max-width: 100%;
+      max-height: 100%;
+      border: 1px solid rgba(255, 255, 255, 0.2);
+    }
+  }
+`;
+
+type ScreenshotProps = {
+  /** path to screenshot image file */
+  readonly image: string;
+}
+
+const Screenshot: React.FC<ScreenshotProps> = ({image}) => {
+  const [ enlarged, setEnlarge ] = useState(false);
+
+  const toggleEnlarge = (event: React.MouseEvent) => {
+    event.preventDefault();
+    setEnlarge(!enlarged);
+  };
+
+  return (
+    <ScreenshotBase onClick={toggleEnlarge}>
+      <div className="screenshot">
+        <img src={image} alt="screenshot" />
+      </div>
+
+      {enlarged &&
+        <div className="lightbox" onClick={toggleEnlarge}>
+          <div className="wrapper">
+            <img src={image} alt="screenshot" />
+          </div>
+        </div>
+      }
+
+    </ScreenshotBase>
+  );
+}
+
+export default Screenshot;
